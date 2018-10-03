51a1a9e0603fc6dfa60b0924c3ae64cbe20563eb
Adam <awe@it-minds.dk>
2018-10-03 16:04:00 +0200

    Updates

---
+++ precache-manifest.8eaf60500be28612594da16eccc9adac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddimage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "72acbbef05d65fcd185e",
+    "url": "/reddimage/static/js/runtime~main.72acbbef.js"
+  },
+  {
+    "revision": "e285a4789b79c9fd03ad",
+    "url": "/reddimage/static/js/main.e285a478.chunk.js"
+  },
+  {
+    "revision": "8b5011b880174705582f",
+    "url": "/reddimage/static/js/1.8b5011b8.chunk.js"
+  },
+  {
+    "revision": "e285a4789b79c9fd03ad",
+    "url": "/reddimage/static/css/main.601abaeb.chunk.css"
+  },
+  {
+    "revision": "2e4d0d4acf006f2b003c2a0294457ec3",
+    "url": "/reddimage/index.html"
+  }
+];
